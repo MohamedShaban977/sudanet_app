77f06caf9c4516385c3f76c6e5bb2b24d968cead
Mohamd Shaban <shban977@gmail.com>
2023-05-15 21:58:41 +0300

    handle responsive ui educational_levels_screen

--- lib/features/educational_levels/presentation/screens/educational_levels_screen.dart
+++ lib/features/educational_levels/presentation/screens/educational_levels_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:sudanet_app/core/app_manage/extension_manager.dart';
 import 'package:sudanet_app/core/app_manage/strings_manager.dart';
 import 'package:sudanet_app/core/locale/app_localizations.dart';
+import 'package:sudanet_app/core/responsive/responsive.dart';
 
 import '../../../../core/app_manage/color_manager.dart';
 
@@ -24,70 +25,58 @@ class EducationalLevelsScreen extends StatelessWidget {
               child: Text(
                 AppStrings.viewAllEducationalLevels.tr(),
                 style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                    color: ColorManager.primary,),
+                      color: ColorManager.primary,
+                    ),
               ),
             ),
-            Column(
-              children: List.generate(
-                  10,
-                  (index) => Card(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 20.0, vertical: 10.0),
-                        elevation: 8.0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0)),
-                        clipBehavior: Clip.antiAlias,
-                        child: IntrinsicHeight(
-                          child: Row(
-                            children: [
-                              Expanded(
-                                flex: 3,
-                                child: IntrinsicHeight(
-                                  child: Image.network(
-                                    'https://images.pexels.com/photos/13650913/pexels-photo-13650913.jpeg?auto=compress&cs=tinysrgb&w=600',
-                                    fit: BoxFit.fill,
-                                    height: 130,
-                                    gaplessPlayback: true,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                  flex: 5,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          'المرحلة ${index + 1}',
-                                          style: context.displayLarge.copyWith(color: ColorManager.primary),
-                                        ),
-                                        Text(
-                                          '6 مواد',
-                                          style: context.displaySmall.copyWith(color: ColorManager.textGray),
-                                        ),
-                                        SizedBox(
-                                          width: context.width,
-                                          height: 50.0,
-                                          child: ElevatedButton(
+            // Column(
+            //   children: List.generate(
+            //     10,
+            //     (index) => const CardEducationalLevelsWidget(),
+            //   ),
+            // ),
+            GridView.count(
+              crossAxisCount:
+                  Responsive.isMobile(context) || Responsive.isMobileS(context)
+                      ? 1
+                      : Responsive.isTablet(context)
+                          ? 2
+                          : 3,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+              shrinkWrap: true,
+              childAspectRatio: Responsive.isMobile(context)
+                  ? 2.6
+                  : Responsive.isMobileS(context)
+                      ? 2.0
+                      : Responsive.isTablet(context)
+                          ? 1.1
+                          : 1.0,
 
-                                              onPressed: () {
-                                                ///TODO:
-                                              },
-                                              child:  Text(
-                                                  AppStrings.findAvailableSubjects.tr())),
-                                        ),
-                                      ],
-                                    ),
-                                  )),
-                            ],
-                          ),
-                        ),
-                      )),
-            )
+              // Responsive.isMobile(context) ? 2.6 : 1.3,
+              // padding: const EdgeInsets.all(4.0),
+              physics: const ClampingScrollPhysics(),
+              children: List.generate(
+                10,
+                (index) => LayoutBuilder(
+                    builder: (context, BoxConstraints constraints) {
+                  return Responsive(
+                    mobile: CardEducationalLevelsRowWidget(
+                      index: index,
+                      height: constraints.maxHeight,
+                    ),
+                    tablet: CardEducationalLevelsColumnWidget(
+                      index: index,
+                      width: constraints.maxWidth,
+                    ),
+                    desktop: CardEducationalLevelsColumnWidget(
+                      index: index,
+                      width: constraints.maxWidth,
+                    ),
+                  );
+                }),
+              ),
+            ),
           ],
         ),
       ),
@@ -104,8 +93,166 @@ class EducationalLevelsScreen extends StatelessWidget {
         statusBarIconBrightness: Brightness.dark,
         statusBarBrightness: Brightness.dark,
       ),
-      title:
-          Text(AppStrings.educationalLevels.tr(), style: context.displayLarge.copyWith(color: ColorManager.textGray,fontWeight: FontWeight.w700)),
+      title: Text(AppStrings.educationalLevels.tr(),
+          style: context.displayLarge.copyWith(
+              color: ColorManager.textGray, fontWeight: FontWeight.w700)),
+    );
+  }
+}
+
+class CardEducationalLevelsRowWidget extends StatelessWidget {
+  final int index;
+  final double height;
+  final double? width;
+
+  const CardEducationalLevelsRowWidget({
+    super.key,
+    required this.index,
+    required this.height,
+    this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      clipBehavior: Clip.antiAlias,
+      child: IntrinsicHeight(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: IntrinsicHeight(
+                child: Image.network(
+                  'https://images.pexels.com/photos/13650913/pexels-photo-13650913.jpeg?auto=compress&cs=tinysrgb&w=600',
+                  fit: BoxFit.fill,
+                  height: height,
+                  width: width,
+                  gaplessPlayback: true,
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ViewInfoDataEducationalWidget(index: index),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ViewInfoDataEducationalWidget extends StatelessWidget {
+  const ViewInfoDataEducationalWidget({
+    super.key,
+    required this.index,
+  });
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'المرحلة ${index + 1}',
+          style: context.displayLarge.copyWith(color: ColorManager.primary),
+        ),
+        Text(
+          '6 مواد',
+          style: context.displaySmall.copyWith(color: ColorManager.textGray),
+        ),
+        SizedBox(
+          width: context.width,
+          height: 50.0,
+          child: ElevatedButton(
+              onPressed: () {
+                ///TODO:
+              },
+              child: Text(AppStrings.findAvailableSubjects.tr())),
+        ),
+      ],
+    );
+  }
+}
+
+class CardEducationalLevelsColumnWidget extends StatelessWidget {
+  final int index;
+  final double? height;
+  final double? width;
+
+  const CardEducationalLevelsColumnWidget({
+    super.key,
+    required this.index,
+    this.height,
+    this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      clipBehavior: Clip.antiAlias,
+      child: IntrinsicHeight(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: IntrinsicHeight(
+                child: Image.network(
+                  'https://images.pexels.com/photos/13650913/pexels-photo-13650913.jpeg?auto=compress&cs=tinysrgb&w=600',
+                  fit: BoxFit.fill,
+                  // height: height,
+                  width: width,
+                  gaplessPlayback: true,
+                ),
+              ),
+            ),
+            Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Spacer(),
+                      Text(
+                        'المرحلة ${index + 1}',
+                        style: context.displayLarge
+                            .copyWith(color: ColorManager.primary),
+                      ),
+                      const SizedBox(height: 5.0),
+                      Text(
+                        '6 مواد',
+                        style: context.displaySmall
+                            .copyWith(color: ColorManager.textGray),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        width: context.width,
+                        height: 50.0,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              ///TODO:
+                            },
+                            child: Text(AppStrings.findAvailableSubjects.tr())),
+                      ),
+                    ],
+                  ),
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
